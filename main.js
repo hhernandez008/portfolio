f557dc7660c8ec0adb7497c7fa2dd1187be731b2
hhernandez008 <hhernandez008@gmail.com>
2015-11-08 23:02:59 -0800

    added click function to hide/show and animate certificate logos

--- main.js
+++ main.js
@@ -9,5 +9,23 @@
 
 //Start when document is done loading
 $(document).ready(function(){
-    console.log("ready");
+
+    $(".certs").on("click", function(){
+        var element = $(this).children(".title");
+        if($(this).hasClass("certMove")){
+            $(element).fadeIn(0).removeAttr("style");
+            $(this).removeClass("certMove");
+        }else {
+            $(element).css({
+                "transform-origin": "100% 100%",
+                width: "120",
+                height: "120"
+                //border: "transparent"
+            }).fadeOut(1000);
+            $(this).addClass("certMove");
+        }
+
+    })
+
+
 });
